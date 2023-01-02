f03b01a80c188657e185cbb653464b547c1c2ef1
Danil Krox <deferred@users.noreply.github.com>
2023-01-02 09:38:40 +0000

    Remove -u from rsync-synchronize

--- dot_oh-my-zsh/custom/rsync.zsh
+++ dot_oh-my-zsh/custom/rsync.zsh
@@ -1,5 +1,5 @@
 alias rsync-copy="rsync -av --progress -h"
 alias rsync-move="rsync -av --progress -h --remove-source-files"
+alias rsync-synchronize='rsync -av --progress -h --delete'
 alias rsync-update="rsync -avu --progress -h"
-alias rsync-synchronize="rsync -avu --delete --progress -h"
 

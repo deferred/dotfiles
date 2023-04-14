47e6b8a5f44be804aef17cf177778428f736f455
Danil Krox <deferred@users.noreply.github.com>
2023-04-14 14:35:23 +0400

    Distribute spaces between displays

--- private_dot_config/yabai/executable_setup-spaces.sh
+++ private_dot_config/yabai/executable_setup-spaces.sh
@@ -2,6 +2,7 @@
 
 function destroy_excess_spaces {
     local num_spaces=$1
+    echo "destroying excess spacess till there are $num_spaces"
     for _ in $(yabai -m query --spaces | jq ".[].index | select(. > $num_spaces)"); do
         yabai -m space --destroy $(( $num_spaces + 1 ))
     done
@@ -9,18 +10,38 @@ function destroy_excess_spaces {
 
 function create_missing_spaces {
     local num_spaces=$1
+    echo "creating missing spaces till there are $num_spaces"
     while [ $(yabai -m query --spaces | jq length) -lt $num_spaces ]; do
         yabai -m space --create
     done
 }
 
-function determine_display_index {
-    local display_size=$1
-    if [ "$display_size" = "l" ]; then
-        echo $(yabai -m query --displays | jq 'max_by(.frame.w).index') 
-    else
-        echo $(yabai -m query --displays | jq 'min_by(.frame.w).index') 
-    fi
+function distribute_spaces_between_displays {
+    # get displays sorted by width in descending order
+    local displays=$(yabai -m query --displays | jq 'sort_by(-.frame.w)')
+
+    local num_displays=$(echo $displays | jq 'length')
+    local num_spaces=$( yabai -m query --spaces | jq 'length' )
+    local spaces_per_display=$(( $num_spaces / $num_displays ))
+
+    echo "distributing spaces between displays so there are $spaces_per_display spaces on each display"
+    local space_idx=1
+    for display_idx in $(echo $displays | jq '.[].index'); do
+        local last_space_idx=$(( $space_idx + $spaces_per_display - 1))
+        while [ $space_idx -le $last_space_idx ]; do
+            echo "sending space $space_idx to display $display_idx"
+            yabai -m space "$space_idx" --display "$display_idx"
+            ((space_idx++))
+        done
+    done
+    
+    # send excess spaces to the smallest display
+    local smallest_display_idx=$(echo $displays | jq '.[-1]')
+    while [ $space_idx -lt $num_spaces ]; do
+        echo "sending space $space_idx to display $smallest_display_idx"
+        yabai -m space "$space_idx" --display "$smallest_display_idx"
+        ((space_idx++))
+    done
 }
 
 function setup_spaces {
@@ -28,21 +49,7 @@ function setup_spaces {
 
     destroy_excess_spaces $num_spaces
     create_missing_spaces $num_spaces
-    
-    for i in "web 1 l" "terminal 2 l" "code 3 l" "random 4 l" "slack 5 s" "email 6 s" "teams 7 s" "telegram 8 s"; do
-        # convert the "tuple" into the param args $1 $2...
-        set -- $i 
-        local space_label="$1"
-        local space_idx="$2"
-        local display_idx=$(determine_display_index $3)
-
-        # set labels to corresponding spaces
-        yabai -m space "$space_idx" --label "$space_label"
-
-        # send spaces to corresponding displays
-        echo "sending space $space_label with index $space_idx to display $display_idx"
-        yabai -m space "$space_idx" --display "$display_idx"
-    done
+    distribute_spaces_between_displays
 }
 
 setup_spaces $@

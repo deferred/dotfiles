20c89b90bc0087adc39614d99c0745b5b32dd1a7
Danil Krox <deferred@users.noreply.github.com>
2023-04-17 22:39:14 +0400

    Refactor

--- private_dot_config/yabai/executable_defeat-teams.sh
+++ private_dot_config/yabai/executable_defeat-teams.sh
@@ -1,15 +1,25 @@
 #!/usr/bin/env sh
 
-echo "Running $(basename "$0")"
-focused_app=$(yabai -m query --windows --window)
-is_invis_teams_noti=$(echo "$focused_app" | jq '.app == "Microsoft Teams" and .level == 3')
-echo "Invisible teams window selected: $is_invis_teams_noti"
+function hide_teams_popup {
+    local focused_window=$(yabai -m query --windows --window)
+    
+    local is_focused_window_teams_popup=$(echo "$focused_window" | jq '.app == "Microsoft Teams" and .level == 3')
+    if $is_focused_window_teams_popup; then
+        local window_id=$(echo "$focused_window" | jq '.id')
+        echo "minimizing Microsoft Teams popup window $window_id" 
+        yabai -m window $window_id --minimize
 
-if $is_invis_teams_noti; then
-        focused_display_id=$(echo "$focused_app" | jq '.display')
-        visible_space_id=$(yabai -m query --spaces --display $focused_display_id | jq 'map(select(."is-visible" == true))[0].index')
-	available_windows=$(yabai -m query --windows --display $focused_display_id | jq --argjson space $visible_space_id 'map(select(.space == $space))')
-    yabai -m window --minimize
-	recent_window_id=$(echo "$available_windows" | jq 'map(select(.title | contains("Microsoft Teams") | not))[0].id')
-	yabai -m window --focus $recent_window_id
-fi
+        local focused_display_id=$(echo "$focused_window" | jq '.display')
+        local visible_space_id=$(yabai -m query --spaces --display $focused_display_id | jq 'map(select(."is-visible" == true))[0].index')
+        local available_windows=$(yabai -m query --windows --display $focused_display_id | jq --argjson space $visible_space_id 'map(select(.space == $space))')
+        local recent_window_id=$(echo "$available_windows" | jq 'map(select(.title | contains("Microsoft Teams") | not))[0].id // empty')
+        if [[ ! -z "$recent_window_id" ]]; then
+            echo "focusing window $recent_window_id instead of Microsoft Teams popup"
+            yabai -m window  "$recent_window_id" --focus
+        fi
+    fi
+}
+
+echo "running $(basename "$0")"
+hide_teams_popup $@
+

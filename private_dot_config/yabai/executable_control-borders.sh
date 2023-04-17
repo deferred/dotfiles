9ee3c80d5ed201dd55b1e28d9843f5bfa7b47030
Danil Krox <deferred@users.noreply.github.com>
2023-04-17 23:45:54 +0400

    Refactor

--- private_dot_config/yabai/executable_control-borders.sh
+++ private_dot_config/yabai/executable_control-borders.sh
@@ -1,12 +1,21 @@
 #!/usr/bin/env bash
 
-windows="$(yabai -m query --spaces --space | jq '.windows')"
+function control_window_borders {
+    local multiple_windows_visible=$(yabai -m query --windows --space | jq 'map(select(."is-visible" == true)) | length > 1')
+    if $multiple_windows_visible
+    then
+        echo "turning window border on because multiple windows are visible on focused space"
+        yabai -m config window_border on
+        yabai -m config window_border_hidpi on
+        yabai -m config window_border_width 2
+        yabai -m config active_window_border_color "0xFF50FA7B"
+        # transparent
+        yabai -m config normal_window_border_color "0x00FFFFFF"
+    else
+        echo "turning window border off"
+        yabai -m config window_border off
+    fi
+}
 
-if [[ $windows == *","* ]]
-then
-  yabai -m config window_border on
-  yabai -m config active_window_border_color "0xFFFFFF00"
-else
-  yabai -m config window_border off
-fi
-
+echo "running $(basename "$0")"
+control_window_borders $@

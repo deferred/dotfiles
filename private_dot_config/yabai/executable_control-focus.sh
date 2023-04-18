ae0cf5172ba134f415cd022157927c0f87dbcf5f
Danil Krox <deferred@users.noreply.github.com>
2023-04-18 23:07:18 +0400

    Minimize Microsoft Teams popup

--- private_dot_config/yabai/executable_control-focus.sh
+++ private_dot_config/yabai/executable_control-focus.sh
@@ -1,15 +1,7 @@
 #!/usr/bin/env sh
 
 function control_focus {
-    windows=$(yabai -m query --windows --space)
-    
-    local teams_popup_window_id=$(echo "$windows" | jq '.[] | select(.app == "Microsoft Teams" and .frame.w == 304 and .frame.h == 247) | .id // empty')
-    if [[ ! -z $teams_popup_window_id ]]; then
-        echo "minimizing Microsoft Teams popup window $teams_popup_window_id" 
-        yabai -m window $teams_popup_window_id --minimize
-    fi 
-    
-    local window_id=$(echo "$windows" | jq 'map(select(.title | contains("Microsoft Teams") | not))[0].id // empty')
+    local window_id=$(yabai -m query --windows --space | jq 'map(select(.title | contains("Microsoft Teams") | not))[0].id // empty')
     if [[ ! -z $window_id ]]; then
         echo "focusing window $window_id"
         yabai -m window --focus $window_id

29caaa78d37b6d585fd03b8c1afa69b417e5ec84
Danil Krox <deferred@users.noreply.github.com>
2023-04-24 18:34:56 +0400

    Control borders on space change

--- private_dot_config/yabai/executable_control-borders.sh
+++ private_dot_config/yabai/executable_control-borders.sh
@@ -1,14 +1,12 @@
 #!/usr/bin/env bash
 
 function control_window_borders {
-    local multiple_windows_visible=$(yabai -m query --windows --space | jq 'map(select(."is-visible" == true)) | length > 1')
-    if $multiple_windows_visible
-    then
-        echo "turning window border on because multiple windows are visible on focused space"
+    local multiple_windows_visible=$(yabai -m query --windows --space | jq 'map(select(."is-visible" == true and (.title | contains("Microsoft Teams Notification") | not))) | length > 1')
+    if $multiple_windows_visible; then
+        echo "turning window border on"
         yabai -m config window_border on
         yabai -m config window_border_hidpi on
-        yabai -m config window_border_width 2
-        yabai -m config active_window_border_color "0xFF50FA7B"
+        yabai -m config active_window_border_color "0xFFF8F8F2"
         # transparent
         yabai -m config normal_window_border_color "0x00FFFFFF"
     else

8ecc53a54488fe04e5ad673a042332e66cae09ad
Danil Krox <deferred@users.noreply.github.com>
2023-05-15 00:05:47 +0400

    Override buffer search initial mode to insert

--- private_dot_config/lvim/config.lua
+++ private_dot_config/lvim/config.lua
@@ -1,6 +1,7 @@
 require("user.lualine")
 require("user.options")
 require("user.plugins")
+require("user.telescope")
 require("user.which_key")
 
 lvim.colorscheme = "dracula"
---
+++ private_dot_config/lvim/lua/user/telescope.lua
@@ -0,0 +1,3 @@
+-- Override buffer search initial mode to insert
+lvim.builtin.telescope.pickers.buffers.initial_mode = "insert"
+

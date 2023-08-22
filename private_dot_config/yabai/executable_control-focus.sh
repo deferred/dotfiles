97c8d773760b403c4ee142f9c1d4849ff10cc740
Danil Krox <deferred@users.noreply.github.com>
2023-08-22 15:43:11 +0200

    Control focus when there are floating windows on screen

--- private_dot_config/yabai/executable_control-focus.sh
+++ private_dot_config/yabai/executable_control-focus.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env sh
 
 function control_focus {
-    local window_id=$(yabai -m query --windows --space | jq 'map(select(.title | contains("Microsoft Teams") | not))[0].id // empty')
+    local window_id=$(yabai -m query --windows --space | jq 'map(select(."is-floating" | not))[0].id // empty')
     if [[ ! -z $window_id ]]; then
         echo "focusing window $window_id"
         yabai -m window --focus $window_id

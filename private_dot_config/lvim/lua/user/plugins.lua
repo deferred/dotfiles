26cc70447256dc804aaa9389557d3fc0ac786a4a
Danil Krox <deferred@users.noreply.github.com>
2023-11-22 20:21:56 +0100

    Install hardtime.nvim

--- private_dot_config/lvim/lua/user/plugins.lua
+++ private_dot_config/lvim/lua/user/plugins.lua
@@ -2,4 +2,9 @@ lvim.plugins = {
     { 'dracula/vim', name = 'dracula' },
     "AckslD/swenv.nvim",
     "stevearc/dressing.nvim",
+    {
+        "m4xshen/hardtime.nvim",
+        dependencies = { "MunifTanjim/nui.nvim", "nvim-lua/plenary.nvim" },
+        opts = {}
+    },
 }

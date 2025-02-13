98c1b7278fe08dcb4f13c555c4a9ee0b4b741d7c
Danil Krox <deferred@users.noreply.github.com>
2025-02-13 12:45:33 +0100

    Install eza instead of exa

--- dot_oh-my-zsh/custom/exa.zsh
+++
@@ -1,8 +0,0 @@
-alias l="exa -lahg"
-alias la="exa -lahg"
-alias ll="exa -lhg"
-alias ls="exa"
-alias lsa="exa -lahg"
-
-alias tree="exa --tree"
-
---
+++ dot_oh-my-zsh/custom/eza.sh
@@ -0,0 +1,8 @@
+alias l="eza -lahg"
+alias la="eza -lahg"
+alias ll="eza -lhg"
+alias ls="eza"
+alias lsa="eza -lahg"
+
+alias tree="eza --tree"
+

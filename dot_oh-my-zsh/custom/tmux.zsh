d3949f3600605b42038375fdb8eb517b5ab2634a
Danil Krox <deferred@users.noreply.github.com>
2025-02-15 13:25:04 +0100

    Add tsa

--- dot_oh-my-zsh/custom/tmux.zsh
+++ dot_oh-my-zsh/custom/tmux.zsh
@@ -1,2 +1,13 @@
+tsa() {
+    session_name="$1"
+    if [ -z "$session_name" ]; then
+        echo "Usage: tmux-session <session-name>"
+        return 1
+    fi
+    tmux attach-session -t "$session_name" 2>/dev/null || tmux new-session -s "$session_name"
+}
+
+
 ZSH_TMUX_CONFIG="$HOME/.config/tmux/tmux.conf"
+
 

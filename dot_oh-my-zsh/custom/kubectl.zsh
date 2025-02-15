02fcb5f7ba0955f05bee148ffe21f2f546c02682
Danil Krox <deferred@users.noreply.github.com>
2025-02-15 13:25:24 +0100

    Add fkdelp

--- dot_oh-my-zsh/custom/kubectl.zsh
+++ dot_oh-my-zsh/custom/kubectl.zsh
@@ -1,1 +1,2 @@
-alias fkdp="kubectl get pods --all-namespaces --no-headers | fzf | awk '{print \$2, \$1}' | xargs -n 2 sh -c 'kubectl describe pod \$0 -n \$1'"
+alias fkdp="kubectl get pods --all-namespaces --no-headers -o wide | fzf | awk '{print \$2, \$1}' | xargs -n 2 sh -c 'kubectl describe pod \$0 -n \$1'"
+alias fkdelp="kubectl get pods --all-namespaces --no-headers -o wide | fzf | awk '{print \$2, \$1}' | xargs -n 2 sh -c 'kubectl delete pod \$0 -n \$1'"

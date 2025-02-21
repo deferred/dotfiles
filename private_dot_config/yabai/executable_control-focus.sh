4b72d070f385705a68903b3ee1d5310217be0478
Danil Krox <deferred@users.noreply.github.com>
2025-02-21 16:50:28 +0100

    Remove comment

--- private_dot_config/yabai/executable_control-focus.sh
+++ private_dot_config/yabai/executable_control-focus.sh
@@ -9,20 +9,15 @@ control_focus() {
 
   # Extract the first window ID whose title does NOT contain "Microsoft Teams"
   local window_id
-  #  1) map(...)             : Transform (map) each element of the array.
-  #  2) select(...)          : Select only those items whose "title" does NOT contain "Microsoft Teams".
-  #  3) [0].id               : Take the first matching item ([0]) and extract its "id".
-  #  4) // ""                : If there is no item (no matching window), default to an empty string.
-window_id="$(
-  jq -r '
-    map(
-      select(
-        (.title | contains("Microsoft Teams") | not)
-        and (.title | contains("Slack") | not)
-      )
-    )[0].id // ""
-  ' <<< "$windows"
-)"
+  window_id="$(
+    jq -r '
+      map(
+        select(
+          .app != "Microsoft Teams" and .app != "Slack"
+        )
+      )[0].id // ""
+    ' <<< "$windows"
+  )"
 
   # If a valid window ID was found, focus it
   if [[ -n "$window_id" ]]; then

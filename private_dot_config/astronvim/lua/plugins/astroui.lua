6e7154b585704fbed62f486636b3cfe5cc164fa4
Danil Krox <deferred@users.noreply.github.com>
2025-08-22 16:50:18 +0200

    Format

--- private_dot_config/astronvim/lua/plugins/astroui.lua
+++ private_dot_config/astronvim/lua/plugins/astroui.lua
@@ -5,37 +5,37 @@
 
 ---@type LazySpec
 return {
-  "AstroNvim/astroui",
-  ---@type AstroUIOpts
-  opts = {
-    -- change colorscheme
-    colorscheme = "dracula",
-    -- AstroUI allows you to easily modify highlight groups easily for any and all colorschemes
-    highlights = {
-      init = {                         -- this table overrides highlights in all themes
-        -- Normal = { bg = "#000000" },
-        StatusLine = { bg = "#44475a" }, -- change statusline background
-        StatusLineNC = { bg = "#282a36" }, -- non-current window statusline
-        WinBar = { bg = "NONE" },      -- remove breadcrumbs background
-        WinBarNC = { bg = "NONE" },    -- remove non-current breadcrumbs background
-      },
-      astrodark = {                    -- a table of overrides/changes when applying the astrotheme theme
-        -- Normal = { bg = "#000000" },
-      },
-    },
-    -- Icons can be configured throughout the interface
-    icons = {
-      -- configure the loading of the lsp in the status line
-      LSPLoading1 = "⠋",
-      LSPLoading2 = "⠙",
-      LSPLoading3 = "⠹",
-      LSPLoading4 = "⠸",
-      LSPLoading5 = "⠼",
-      LSPLoading6 = "⠴",
-      LSPLoading7 = "⠦",
-      LSPLoading8 = "⠧",
-      LSPLoading9 = "⠇",
-      LSPLoading10 = "⠏",
-    },
-  },
+	"AstroNvim/astroui",
+	---@type AstroUIOpts
+	opts = {
+		-- change colorscheme
+		colorscheme = "dracula",
+		-- AstroUI allows you to easily modify highlight groups easily for any and all colorschemes
+		highlights = {
+			init = { -- this table overrides highlights in all themes
+				-- Normal = { bg = "#000000" },
+				StatusLine = { bg = "#44475a" }, -- change statusline background
+				StatusLineNC = { bg = "#282a36" }, -- non-current window statusline
+				WinBar = { bg = "NONE" }, -- remove breadcrumbs background
+				WinBarNC = { bg = "NONE" }, -- remove non-current breadcrumbs background
+			},
+			astrodark = { -- a table of overrides/changes when applying the astrotheme theme
+				-- Normal = { bg = "#000000" },
+			},
+		},
+		-- Icons can be configured throughout the interface
+		icons = {
+			-- configure the loading of the lsp in the status line
+			LSPLoading1 = "⠋",
+			LSPLoading2 = "⠙",
+			LSPLoading3 = "⠹",
+			LSPLoading4 = "⠸",
+			LSPLoading5 = "⠼",
+			LSPLoading6 = "⠴",
+			LSPLoading7 = "⠦",
+			LSPLoading8 = "⠧",
+			LSPLoading9 = "⠇",
+			LSPLoading10 = "⠏",
+		},
+	},
 }

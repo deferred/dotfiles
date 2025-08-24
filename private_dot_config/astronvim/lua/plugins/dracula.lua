2a5c9721699a3c0fb0062ffe261a8c32933aa0d2
Danil Krox <deferred@users.noreply.github.com>
2025-08-24 13:33:45 +0200

    Customize dashboard colors

--- private_dot_config/astronvim/lua/plugins/dracula.lua
+++ private_dot_config/astronvim/lua/plugins/dracula.lua
@@ -11,7 +11,7 @@ return {
         init = {
           -- statusline colors
           StatusLine = { bg = "#44475a" }, -- change statusline background
-          StatusLineNC = { bg = "#282a36" }, -- non-current window statusline
+          StatusLineNC = { bg = "NONE" }, -- non-current window statusline
 
           -- tabline colors
           TabLine = { bg = "#44475a" }, -- change tabline background
@@ -20,6 +20,14 @@ return {
           -- breadcrumb colors
           WinBar = { bg = "NONE" }, -- remove breadcrumbs background
           WinBarNC = { bg = "NONE" }, -- remove non-current breadcrumbs background
+
+          -- dashboard colors
+          SnacksDashboardHeader = { fg = "#bd93f9" }, -- purple for dashboard header/logo
+          SnacksDashboardDesc = { fg = "#f8f8f2" }, -- foreground for menu descriptions like "New file"
+          SnacksDashboardIcon = { fg = "#ff79c6" }, -- pink for dashboard icons
+          SnacksDashboardKey = { fg = "#8be9fd" }, -- cyan for dashboard keys like "[f]"
+          SnacksDashboardFooter = { fg = "#6272a4" }, -- comment color for footer/startup stats
+          SnacksDashboardSpecial = { fg = "#50fa7b" }, -- green for special dashboard text
         },
       },
     },

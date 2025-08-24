cfe3cda59ca7e3dbaae9b961bb8e1ee16fcae5b7
Danil Krox <deferred@users.noreply.github.com>
2025-08-24 21:16:39 +0200

    Change :w from <leader>w to <leader>s

--- private_dot_config/astronvim/lua/plugins/keybindings.lua
+++ private_dot_config/astronvim/lua/plugins/keybindings.lua
@@ -6,6 +6,10 @@ return {
         -- Swap <leader>fo and <leader>fO keybindings
         ["<Leader>fo"] = { function() require("snacks").picker.recent({ filter = { cwd = true } }) end, desc = "Find old files (cwd)" },
         ["<Leader>fO"] = { function() require("snacks").picker.recent() end, desc = "Find old files" },
+
+        -- Change save hotkey from <leader>w to <leader>s
+        ["<Leader>w"] = false, -- disable default save mapping
+        ["<Leader>s"] = { "<cmd>w<cr>", desc = "Save" },
       },
     },
   },

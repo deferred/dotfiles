85ef09f2486ddeae45f8f979f568af1b8556b637
Danil Krox <deferred@users.noreply.github.com>
2025-08-25 20:22:49 +0200

    Format

--- private_dot_config/astronvim/lua/plugins/keybindings.lua
+++ private_dot_config/astronvim/lua/plugins/keybindings.lua
@@ -1,19 +1,29 @@
 return {
-  "AstroNvim/astrocore",
-  opts = {
-    mappings = {
-      n = {
-        -- Swap <leader>fo and <leader>fO keybindings
-        ["<Leader>fo"] = { function() require("snacks").picker.recent({ filter = { cwd = true } }) end, desc = "Find old files (cwd)" },
-        ["<Leader>fO"] = { function() require("snacks").picker.recent() end, desc = "Find old files" },
+	"AstroNvim/astrocore",
+	opts = {
+		mappings = {
+			n = {
+				-- Swap <leader>fo and <leader>fO keybindings
+				["<Leader>fo"] = {
+					function()
+						require("snacks").picker.recent({ filter = { cwd = true } })
+					end,
+					desc = "Find old files (cwd)",
+				},
+				["<Leader>fO"] = {
+					function()
+						require("snacks").picker.recent()
+					end,
+					desc = "Find old files",
+				},
 
-        -- Change save from <leader>w to <leader>s
-        ["<Leader>s"] = { "<cmd>w<cr>", desc = "Save" },
+				-- Change save from <leader>w to <leader>s
+				["<Leader>s"] = { "<cmd>w<cr>", desc = "Save" },
 
-        -- Change window close from <leader>q to <leader>w
-        ["<Leader>w"] = { "<cmd>close<cr>", desc = "Close window" },
-        ["<Leader>q"] = false, -- disable default quit mapping
-      },
-    },
-  },
+				-- Change window close from <leader>q to <leader>w
+				["<Leader>w"] = { "<cmd>close<cr>", desc = "Close window" },
+				["<Leader>q"] = false, -- disable default quit mapping
+			},
+		},
+	},
 }

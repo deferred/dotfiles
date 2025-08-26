e17cd92523840353601fe7c3983a4397d4d9e42a
Danil Krox <deferred@users.noreply.github.com>
2025-08-26 18:42:14 +0200

    Fix a keybinding collision for <leader>x

--- private_dot_config/astronvim/lua/plugins/keybindings.lua
+++ private_dot_config/astronvim/lua/plugins/keybindings.lua
@@ -20,11 +20,21 @@ return {
 				-- Change save from <leader>w to <leader>s
 				["<Leader>s"] = { "<cmd>w<cr>", desc = "Save" },
 
-				-- Buffer and window management
-				["<Leader>w"] = { "<cmd>bd<cr>", desc = "Close buffer" },
-				["<Leader>W"] = { "<cmd>bd!<cr>", desc = "Force close buffer" },
+				-- Change list mappings from <leader>x to <leader>L
+				["<Leader>xq"] = false,
+				["<Leader>Lq"] = { "<cmd>copen<cr>", desc = "Quickfix List" },
+				["<Leader>xl"] = false,
+				["<Leader>Ll"] = { "<cmd>lopen<cr>", desc = "Location List" },
+
+				-- Change close window from <leadex>q to leader<x>
+				["<Leader>q"] = false,
 				["<Leader>x"] = { "<cmd>close<cr>", desc = "Close window" },
-				["<Leader>q"] = false, -- disable default quit mapping
+
+				-- Change close buffer from <leader>c to <leader>w
+				["<Leader>c"] = false,
+				["<Leader>w"] = { "<cmd>bd<cr>", desc = "Close buffer" },
+				["<Leader>C"] = false,
+				["<Leader>W"] = { "<cmd>bd!<cr>", desc = "Force close buffer" },
 			},
 		},
 	},

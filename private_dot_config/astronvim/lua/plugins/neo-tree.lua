121291033245bc8644e474c72929c75cf16cc77e
Danil Krox <deferred@users.noreply.github.com>
2025-08-30 20:31:05 +0200

    Set neo-tree to show hidden files and dirs

---
+++ private_dot_config/astronvim/lua/plugins/neo-tree.lua
@@ -0,0 +1,15 @@
+---@type LazySpec
+return {
+  "nvim-neo-tree/neo-tree.nvim",
+  opts = {
+    filesystem = {
+      filtered_items = {
+        hide_dotfiles = false,
+        -- hide_gitignored = false,
+        hide_by_name = {
+          ".git",
+        },
+      },
+    },
+  },
+}

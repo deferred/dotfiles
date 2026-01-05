203ceb52becc4d1c0b3e8bd430f2c465e2d9457a
Danil Krox <deferred@users.noreply.github.com>
2026-01-05 22:15:29 +0100

    Revert to default keybindings for save, close window and close buffer

--- private_dot_config/astronvim/lua/plugins/keybindings.lua
+++ private_dot_config/astronvim/lua/plugins/keybindings.lua
@@ -16,25 +16,6 @@ return {
 					end,
 					desc = "Find old files",
 				},
-
-				-- Change save from <leader>w to <leader>s
-				["<Leader>s"] = { "<cmd>w<cr>", desc = "Save" },
-
-				-- Change list mappings from <leader>x to <leader>L
-				["<Leader>xq"] = false,
-				["<Leader>Lq"] = { "<cmd>copen<cr>", desc = "Quickfix List" },
-				["<Leader>xl"] = false,
-				["<Leader>Ll"] = { "<cmd>lopen<cr>", desc = "Location List" },
-
-				-- Change close window from <leadex>q to leader<x>
-				["<Leader>q"] = false,
-				["<Leader>x"] = { "<cmd>close<cr>", desc = "Close window" },
-
-				-- Change close buffer from <leader>c to <leader>w
-				["<Leader>c"] = false,
-				["<Leader>w"] = { "<cmd>bd<cr>", desc = "Close buffer" },
-				["<Leader>C"] = false,
-				["<Leader>W"] = { "<cmd>bd!<cr>", desc = "Force close buffer" },
 			},
 		},
 	},

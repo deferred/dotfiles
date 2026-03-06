3da2c08924c8b634e9ce315eaa705fe4acbb142e
Danil Krox <deferred@users.noreply.github.com>
2026-03-06 17:59:31 +0100

    feat(worktrunk): add aliases

--- home/dot_oh-my-zsh/custom/worktrunk.zsh
+++ home/dot_oh-my-zsh/custom/worktrunk.zsh
@@ -3,3 +3,6 @@ if command -v wt >/dev/null 2>&1; then
 fi
 
 alias wts='wt switch --no-cd'
+alias wtc='wt switch --no-cd --create'
+alias wtcb='wt switch --no-cd --no-verify --create'
+alias wtr='wt remove'

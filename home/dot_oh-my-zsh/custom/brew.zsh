0d041e9ca7d1817351dca7ac41bff47a131cbf14
Danil Krox <deferred@users.noreply.github.com>
2026-03-22 20:04:57 +0100

    feat: make brew single user again

--- home/dot_oh-my-zsh/custom/brew.zsh
+++ home/dot_oh-my-zsh/custom/brew.zsh
@@ -1,9 +1,1 @@
 eval "$(/opt/homebrew/bin/brew shellenv)"
-
-brew() {
-  if [[ "$1" == "services" ]]; then
-    command brew "$@"
-  else
-    (cd /opt/homebrew && sudo -Hu homebrew brew "$@")
-  fi
-}

94b23594a9ae9f03e28ffafd7241987a972620af
Danil Krox <deferred@users.noreply.github.com>
2026-04-01 21:57:59 +0200

    chore(worktrunk): add wtsd alias to switch to default worktree

--- home/dot_oh-my-zsh/custom/worktrunk.zsh
+++ home/dot_oh-my-zsh/custom/worktrunk.zsh
@@ -3,6 +3,7 @@ if command -v wt >/dev/null 2>&1; then
 fi
 
 alias wts='wt switch --no-cd'
+alias wtsd='wt switch --no-cd ^'
 alias wtc='wt switch --no-cd --create'
 alias wtcb='wt switch --no-cd --no-verify --create'
 alias wtr='wt remove'
